6367594a29f5001fa5ce498ca40bc9501be663a3
Virtuous-Viraj <virajnerlekar1729@gmail.com>
2023-04-25 09:15:18 +0530

    backend changes: ready for deployment

--- config/db.js
+++ config/db.js
@@ -3,8 +3,8 @@ const mongoose = require('mongoose')
 
 const connectDB = async() =>{
     try {
-        console.log(process.env.MONGO_URI+"eDocQuick_DB_")
-        await mongoose.connect(process.env.MONGO_URI+"eDocQuick_DB_",{
+        console.log(process.env.MONGO_URI)
+        await mongoose.connect(process.env.MONGO_URI,{
             useNewUrlParser : true,
             useUnifiedTopology : true,  
         })
